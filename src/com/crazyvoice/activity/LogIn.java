1b1fc9cda632a4f70b35e3a7692907dc62375074
yujie <158392613@qq.com>
2016-11-18 16:38:50 +0800

    11.18
    登陆界面优化大部分完成，感觉需要加些元素，以使得界面功能更加完整美观。

--- src/com/crazyvoice/activity/LogIn.java
+++ src/com/crazyvoice/activity/LogIn.java
@@ -14,6 +14,9 @@ import com.crazyvoice.util.ClientConServer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,16 +36,26 @@ public class LogIn extends Activity implements OnClickListener {
 	private EditText passwordEditText;
 	private Button loginButton;
 	private Button registButton;
-	private Button testButton;
-	private Button testButton2;
+	private Button forgetPwdButton;
+	private Button userNameClear;
+	private Button pwdClear;
+	private Button pwsEye;
+	//根据编辑框变化从而做出的动作
+	private TextWatcher username_watcher;
+	private TextWatcher password_watcher; 
+//	private Button testButton;
+//	private Button testButton2;
 	private boolean loginResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_log_in);
+		setContentView(R.layout.activity_login);
 		init();
+		initWatcher();
+		accountEditText.addTextChangedListener(username_watcher);
+		passwordEditText.addTextChangedListener(password_watcher);
 	}
 
 	/**
@@ -52,7 +65,7 @@ public class LogIn extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		// 登录按钮点击事件，目标是跳转到频道获取页面
-		if (v.getId() == R.id.login_button) {
+		if (v.getId() == R.id.login) {
 			String account = accountEditText.getText().toString().trim();
 			String password = passwordEditText.getText().toString().trim();
 			if (account.equals("") || password.equals("")) {
@@ -71,19 +84,33 @@ public class LogIn extends Activity implements OnClickListener {
 					Toast.makeText(LogIn.this, "Fail", Toast.LENGTH_SHORT).show();
 				}
 			}
-		} else if (v.getId() == R.id.enter_regist_button) {// 注册按钮点击事件，跳转到注册页面
+		} else if (v.getId() == R.id.button_register) {// 注册按钮点击事件，跳转到注册页面
 			Intent intent = new Intent();
 			intent.setClass(LogIn.this, RegistActicity.class);
 			startActivity(intent);
-		}else if(v.getId() == R.id.Test_button){
-			Intent intent = new Intent();
-			intent.setClass(LogIn.this, queryServerRoomTest.class);
-			startActivity(intent);
-		}else if(v.getId() == R.id.Test2_button){
-			Intent intent = new Intent();
-			intent.setClass(LogIn.this, CreatServerRoomTest.class);
-			startActivity(intent);
+		}else if(v.getId() == R.id.button_forget_pwd){//忘记密码界面
+//			Intent intent = new Intent();
+//			intent.setClass(LogIn.this, queryServerRoomTest.class);
+//			startActivity(intent);
+		}else if(v.getId() == R.id.button_username_clear){//清除用户名
+			accountEditText.setText(null);
+		}else if(v.getId() == R.id.button_pwd_clear){//清除密码
+			passwordEditText.setText(null);
+		}else if(v.getId() == R.id.button_pwd_eye){//设置密码是否可见
+			if(passwordEditText.getInputType() == (InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD)){
+				pwsEye.setBackgroundResource(R.drawable.button_eye);
+			    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_NORMAL);
+			   }else{
+				pwsEye.setBackgroundResource(R.drawable.button_eye_closed);
+			    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			   }
+			passwordEditText.setSelection(passwordEditText.getText().toString().length());
 		}
+//		else if(v.getId() == R.id.Test2_button){
+//			Intent intent = new Intent();
+//			intent.setClass(LogIn.this, CreatServerRoomTest.class);
+//			startActivity(intent);
+//		}
 		
 	}
 
@@ -91,19 +118,85 @@ public class LogIn extends Activity implements OnClickListener {
 	 * 初始化组件
 	 */
 	public void init() {
-		accountEditText = (EditText) findViewById(R.id.username);
-		passwordEditText = (EditText) findViewById(R.id.password);
-		loginButton = (Button) findViewById(R.id.login_button);
-		registButton = (Button) findViewById(R.id.enter_regist_button);
-		testButton=(Button) findViewById(R.id.Test_button);
-		testButton2=(Button) findViewById(R.id.Test2_button);
+		accountEditText = (EditText) findViewById(R.id.edit_username);
+		passwordEditText = (EditText) findViewById(R.id.edit_password);
+		loginButton = (Button) findViewById(R.id.login);
+		registButton = (Button) findViewById(R.id.button_register);
+		forgetPwdButton=(Button) findViewById(R.id.button_forget_pwd);
+		userNameClear=(Button) findViewById(R.id.button_username_clear);
+		pwdClear=(Button) findViewById(R.id.button_pwd_clear);
+		pwsEye=(Button) findViewById(R.id.button_pwd_eye);
+//		testButton=(Button) findViewById(R.id.Test_button);
+//		testButton2=(Button) findViewById(R.id.Test2_button);
 		loginButton.setOnClickListener(this);
 		registButton.setOnClickListener(this);
-		testButton.setOnClickListener(this);
-		testButton2.setOnClickListener(this);
+		forgetPwdButton.setOnClickListener(this);
+		userNameClear.setOnClickListener(this);
+		pwdClear.setOnClickListener(this);
+		pwsEye.setOnClickListener(this);
+//		testButton.setOnClickListener(this);
+//		testButton2.setOnClickListener(this);
 		SmackAndroid.init(LogIn.this);// 初始化Asmack平台,必须的，否则会报错
 	}
-
+	
+	 /**
+	  * 用户名，密码输入控件公用这一个watcher
+	  */
+	private void initWatcher() {
+		// TODO Auto-generated method stub
+		username_watcher=new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				//passwordEditText.setText(null);
+				if(s.toString().length()>0){
+					userNameClear.setVisibility(View.VISIBLE);
+				}else{
+					userNameClear.setVisibility(View.INVISIBLE);
+				}
+			}
+		};
+		password_watcher=new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				if(s.toString().length()>0){
+					pwdClear.setVisibility(View.VISIBLE);
+				}else{
+					pwdClear.setVisibility(View.INVISIBLE);
+				}
+			}
+		};
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
