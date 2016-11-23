3a3763bb3a7f2dbaf83ae13826e1692710a9210b
yujie <158392613@qq.com>
2016-11-23 17:23:02 +0800

    11.23-下班
    从聚合数据获取的节目单json数据处理方法创建完成，返回当前正在播放的节目方法有待完成。

---
+++ src/com/crazyvoice/model/Program.java
@@ -0,0 +1,33 @@
+package com.crazyvoice.model;
+
+public class Program {
+	private String cName;
+	private String pName;
+	private String pUrl;
+	private String time;
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getpUrl() {
+		return pUrl;
+	}
+	public void setpUrl(String pUrl) {
+		this.pUrl = pUrl;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
--- src/com/crazyvoice/util/Utility.java
+++ src/com/crazyvoice/util/Utility.java
@@ -1,8 +1,10 @@
 package com.crazyvoice.util;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
@@ -10,11 +12,15 @@ import android.util.Log;
 import com.crazyvoice.db.CrazyVoiceDB;
 import com.crazyvoice.model.Category;
 import com.crazyvoice.model.Channel;
+import com.crazyvoice.model.Program;
 
-/*
+/**
+ * 
  * 处理服务器返回数据
  */
 public class Utility {
+	public static List<Program> programs;
+
 	/*
 	 * 处理频道分类信息
 	 */
@@ -22,8 +28,8 @@ public class Utility {
 			CrazyVoiceDB crazyVoiceDB, String reponse) {
 		try {
 			JSONObject object = new JSONObject(reponse);
-			Log.d("CrazyVoice_Utility_Category",
-					object.get("error_code") + ":" + object.get("reason"));
+			Log.d("CrazyVoice_Utility_Category", object.get("error_code") + ":"
+					+ object.get("reason"));
 			if (object.getInt("error_code") == 0) {
 				String result = object.getString("result");
 				Log.d("CrazyVoice_Utility_Category", result);
@@ -39,8 +45,8 @@ public class Utility {
 				}
 				return true;
 			} else {
-				Log.d("CrazyVoice_Utility_Category", object.get("error_code") + ":"
-						+ object.get("reason"));
+				Log.d("CrazyVoice_Utility_Category", object.get("error_code")
+						+ ":" + object.get("reason"));
 				return false;
 			}
 		} catch (Exception e) {
@@ -58,8 +64,8 @@ public class Utility {
 			String reponse) {
 		try {
 			JSONObject object = new JSONObject(reponse);
-			Log.d("CrazyVoice_Utility_Channel",
-					object.get("error_code") + ":" + object.get("reason"));
+			Log.d("CrazyVoice_Utility_Channel", object.get("error_code") + ":"
+					+ object.get("reason"));
 			if (object.getInt("error_code") == 0) {
 				String result = object.getString("result");
 				Log.d("CrazyVoice_Utility_Channel", result);
@@ -69,18 +75,18 @@ public class Utility {
 					String rel = jsonObject.getString("rel");
 					String channelName = jsonObject.getString("channelName");
 					String pid = jsonObject.getString("pId");
-					//String url=jsonObject.getString("url");
+					// String url=jsonObject.getString("url");
 					Channel channel = new Channel();
 					channel.setChannelCode(rel);
 					channel.setCategoryId(Integer.parseInt(pid));
 					channel.setChannelName(channelName);
-					//channel.setUrl(channelName);
+					// channel.setUrl(channelName);
 					crazyVoiceDB.saveChannel(channel);
 				}
 				return true;
 			} else {
-				Log.d("CrazyVoice_Utility_Channel", object.get("error_code") + ":"
-						+ object.get("reason"));
+				Log.d("CrazyVoice_Utility_Channel", object.get("error_code")
+						+ ":" + object.get("reason"));
 				return false;
 			}
 		} catch (Exception e) {
@@ -89,5 +95,55 @@ public class Utility {
 		}
 		return false;
 
+	}
+
+	/*
+	 * 处理节目信息
+	 */
+	public static boolean handleProgramResponse(CrazyVoiceDB crazyVoiceDB,
+			String reponse) {
+		try {
+			JSONObject object = new JSONObject(reponse);
+			Log.d("CrazyVoice_Utility_Program", object.get("error_code") + ":"
+					+ object.get("reason"));
+			if (object.getInt("error_code") == 0) {
+				String result = object.getString("result");
+				Log.d("CrazyVoice_Utility_Program", result);
+				JSONArray jsonArray = new JSONArray(result);
+				for (int i = 0; i < jsonArray.length(); i++) {
+					JSONObject jsonObject = jsonArray.getJSONObject(i);
+					String cName = jsonObject.getString("cName");
+					String pName = jsonObject.getString("pName");
+					String pUrl = jsonObject.getString("pUrl");
+					String time = jsonObject.getString("time");
+					Program program = new Program();
+					program.setcName(cName);
+					program.setpName(pName);
+					program.setpUrl(pUrl);
+					program.setTime(time);
+					programs.add(program);
+				}
+				return true;
+			} else {
+				Log.d("CrazyVoice_Utility_Channel", object.get("error_code")
+						+ ":" + object.get("reason"));
+				return false;
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
+	
+	/*
+	 * 返回当前正在播放的节目
+	 */
+	public static String getNowPrograme(List<Program> programs){
+		
+		
+		return null;
 	}
 }
